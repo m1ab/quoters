05f7a708abcd6327304f31141b4148b2e09c834f
misha <m.kryukov@protonservice.ru>
2017-02-17 16:12:45 +0300

    part2 - context

---
+++ src/main/java/screensaver/CustomScopeRegistryBeabFactoryPostProcessor.java
@@ -0,0 +1,17 @@
+package screensaver;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by misha on 17.02.17.
+ */
+@Component
+public class CustomScopeRegistryBeabFactoryPostProcessor implements BeanFactoryPostProcessor {
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        beanFactory.registerScope("periodical", new PeriodicalScopeConfigurer());
+    }
+}
---
+++ src/main/java/screensaver/PeriodicalScopeConfigurer.java
@@ -0,0 +1,54 @@
+package screensaver;
+
+import org.apache.commons.math3.util.Pair;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.config.Scope;
+
+import java.time.LocalTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.time.LocalTime.*;
+
+
+/**
+ * Created by misha on 17.02.17.
+ */
+public class PeriodicalScopeConfigurer implements Scope {
+
+    Map<String, Pair<LocalTime, Object>> map = new HashMap<>();
+
+    @Override
+    public Object get(String name, ObjectFactory<?> objectFactory) {
+        if (map.containsKey(name)) {
+            Pair<LocalTime, Object> pair = map.get(name);
+            int secondsSinceLastRequest = now().getSecond() - pair.getKey().getSecond();
+            if (secondsSinceLastRequest > 3) {
+                map.put(name, new Pair(now(), objectFactory.getObject()));
+            }
+        } else {
+            map.put(name, new Pair(now(), objectFactory.getObject()));
+        }
+        return map.get(name).getValue();
+    }
+
+    @Override
+    public Object remove(String s) {
+        return null;
+    }
+
+    @Override
+    public void registerDestructionCallback(String s, Runnable runnable) {
+
+    }
+
+    @Override
+    public Object resolveContextualObject(String s) {
+        return null;
+    }
+
+    @Override
+    public String getConversationId() {
+        return null;
+    }
+}
